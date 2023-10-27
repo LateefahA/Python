68fa7b70c69cf91529336c3f83edf180227e9eaa
LateefahA <61961512+LateefahA@users.noreply.github.com>
2023-10-27 02:33:53 +0200

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,30 @@
+print("Python has three numeric types: int, float, and complex")
+
+#int data type
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#float datatype
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#complex datatype
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#bool datatype
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
