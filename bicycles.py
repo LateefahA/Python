e3b4984487112a94ad2d9d18d236076113553e63
Lateefat Arogundade <61961512+LateefahA@users.noreply.github.com>
2023-12-26 01:34:18 +0000

    Add files via upload

---
+++ bicycles.py
@@ -0,0 +1,10 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+
+print(message)
+
+new_message = f"My first bicycle was a {bicycles[0].title()}"
+print(new_message)
